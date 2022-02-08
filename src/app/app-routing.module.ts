ccfd38ead9eeb6b4046a3f5709780d48cac2ef3a
jdjason569569 <jdjason569569@gmail.com>
2022-02-08 00:40:36 -0500

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { AuthGuard } from './auth/guards/auth.guard';
 
 const routes: Routes = [
   {path: 'auth',  //PARA CARGAR RUTAS HIJAS en modo lazyload
   loadChildren: () => import('./auth/auth.module').then(module => module.AuthModule)},
 
   {path: 'heroes',  //PARA CARGAR RUTAS HIJAS en modo lazyload
-  loadChildren: () => import('./heroes/heroes.module').then(module => module.HeroesModule)},
+  loadChildren: () => import('./heroes/heroes.module').then(module => module.HeroesModule),
+  canLoad:[AuthGuard], canActivate:[AuthGuard]},
 
   {path: '404', component: ErrorPageComponent},
 
